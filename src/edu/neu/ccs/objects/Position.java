240441f6a6c4f0d3299e93443fae677eda897c50
Paras <paraschauhan26@gmail.com>
2014-11-16 14:11:09 -0500

    Implemented Position POJO

--- src/edu/neu/ccs/objects/Position.java
+++ src/edu/neu/ccs/objects/Position.java
@@ -4,17 +4,18 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.hadoop.io.WritableComparable;
 
 /**
- * Sample record:
- * {
- * 	"summary":"Drug Listing for Pharmaceuticals, Annual Report coordinator, specifications and bill of materials.",
- * 	"title":"Sr. Regulatory Affairs Associate",
- * 	"start-date":"1978-02-01",
- * 	"is-current":true,
- * 	"company-name":"Baxter Healthcare"
- * }
+ * Class that holds position information of an employee.
+ * 
+ * Sample record: { "summary":
+ * "Drug Listing for Pharmaceuticals, Annual Report coordinator, specifications and bill of materials."
+ * , "title":"Sr. Regulatory Affairs Associate", "start-date":"1978-02-01",
+ * "is-current":true, "company-name":"Baxter Healthcare" }
  * 
  *
  */
@@ -26,13 +27,15 @@ public class Position implements WritableComparable<Position> {
 	private boolean isCurrent;
 	private String startDate;
 	private String endDate;
-	
-	public Position () {
-		
+	private String sector;
+
+	public Position() {
+
 	}
-	
-	public Position(String summary, String title, String companyName, boolean isCurrent, String startDate, String endDate) {
-		
+
+	public Position(String summary, String title, String companyName,
+			boolean isCurrent, String startDate, String endDate, String sector) {
+
 		super();
 		this.summary = summary;
 		this.title = title;
@@ -40,6 +43,7 @@ public class Position implements WritableComparable<Position> {
 		this.isCurrent = isCurrent;
 		this.startDate = startDate;
 		this.endDate = endDate;
+		this.sector = sector;
 	}
 
 	public String getSummary() {
@@ -89,20 +93,87 @@ public class Position implements WritableComparable<Position> {
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
-	
-	public void readFields(DataInput in) throws IOException {
-		// TODO Auto-generated method stub
-		
+
+	public String getSector() {
+		return sector;
 	}
 
-	public void write(DataOutput out) throws IOException {
-		// TODO Auto-generated method stub
-		
+	public void setSector(String sector) {
+		this.sector = sector;
 	}
 
-	public int compareTo(Position other) {
-		// TODO Auto-generated method stub
-		return 0;
+	public void readFields(DataInput dataInput) throws IOException {
+		this.summary = dataInput.readUTF();
+		this.title = dataInput.readUTF();
+		this.companyName = dataInput.readUTF();
+		this.isCurrent = dataInput.readBoolean();
+		this.startDate = dataInput.readUTF();
+		this.endDate = dataInput.readUTF();
+		this.sector = dataInput.readUTF();
+	}
+
+	public void write(DataOutput dataOutput) throws IOException {
+		dataOutput.writeUTF(summary);
+		dataOutput.writeUTF(title);
+		dataOutput.writeUTF(companyName);
+		dataOutput.writeBoolean(isCurrent);
+		dataOutput.writeUTF(startDate);
+		dataOutput.writeUTF(endDate);
+		dataOutput.writeUTF(sector);
+
+	}
+
+	public int compareTo(Position otherPosition) {
+		return new CompareToBuilder()
+				.append(this.summary, otherPosition.summary)
+				.append(this.title, otherPosition.title)
+				.append(this.companyName, otherPosition.companyName)
+				.append(this.isCurrent, otherPosition.isCurrent)
+				.append(this.startDate, otherPosition.startDate)
+				.append(this.endDate, otherPosition.endDate)
+				.append(this.sector, otherPosition.sector).toComparison();
+	}
+
+	@Override
+	public boolean equals(Object otherObject) {
+		if (otherObject == null) {
+			return false;
+		}
+		if (otherObject == this) {
+			return true;
+		}
+		if (otherObject.getClass() != getClass()) {
+			return false;
+		}
+		Position otherPosition = (Position) otherObject;
+		return new EqualsBuilder().appendSuper(super.equals(otherObject))
+				.append(summary, otherPosition.summary)
+				.append(title, otherPosition.title)
+				.append(companyName, otherPosition.companyName)
+				.append(isCurrent, otherPosition.isCurrent)
+				.append(startDate, otherPosition.startDate)
+				.append(endDate, otherPosition.endDate)
+				.append(sector, otherPosition.sector).isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).append(summary).append(title)
+				.append(companyName).append(isCurrent).append(startDate)
+				.append(endDate).append(sector).toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Summary: ").append(summary).append(",")
+				.append("Title: ").append(title).append(",")
+				.append("CompanyName: ").append(companyName).append(",")
+				.append("IsCurrent: ").append(isCurrent).append(",")
+				.append("StartDate: ").append(startDate).append(",")
+				.append("EndDate: ").append(endDate).append(",")
+				.append("Sector: ").append(sector);
+		return stringBuilder.toString();
 	}
 
 }
