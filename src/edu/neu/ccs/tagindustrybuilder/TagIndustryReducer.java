6af19b035e648d02ca4259dae44a00873950b298
Paras <paraschauhan26@gmail.com>
2014-11-18 17:54:00 -0500

    added set for unique industries emmision

--- src/edu/neu/ccs/tagindustrybuilder/TagIndustryReducer.java
+++ src/edu/neu/ccs/tagindustrybuilder/TagIndustryReducer.java
@@ -1,6 +1,8 @@
 package edu.neu.ccs.tagindustrybuilder;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -10,20 +12,39 @@ import edu.neu.ccs.constants.Constants;
 
 public class TagIndustryReducer extends Reducer<Text, Text, NullWritable, Text> {
 
+	private Set<String> uniqueindusteries;
+	private StringBuffer buffer;
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		uniqueindusteries = new HashSet<String>();
+		buffer = new StringBuffer();
+	}
+
 	@Override
 	protected void reduce(Text key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
 
-		StringBuffer buffer = new StringBuffer();
-		
-		buffer.append(key).append(Constants.COMMA);
-		for (Text text : values) {
-			
-			buffer.append(text).append(Constants.COMMA);
+		for (Text industry : values) {
+			uniqueindusteries.add(industry.toString());
 		}
-		
-		//Value contains the tag,value_list as a comma separated string
-		context.write(NullWritable.get(), new Text(buffer.toString().substring(0, buffer.length() - 1)));
+
+		buffer.append(key).append(Constants.COMMA);
+
+		for (String industry : uniqueindusteries) {
+			buffer.append(industry).append(Constants.COMMA);
+		}
+
+		// Value contains the tag,value_list as a comma separated string
+		context.write(NullWritable.get(),
+				new Text(buffer.toString().substring(0, buffer.length() - 1)));
+
+		// Clearing buffer
+		buffer.delete(0, buffer.length());
+
+		// Clearing Set
+		uniqueindusteries.clear();
 	}
 
 }
