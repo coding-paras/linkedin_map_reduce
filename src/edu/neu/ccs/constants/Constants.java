11cabc5256d970b9226f1f3dbd86b3235f0e5884
Phani Dharmavarapu <phanidv88@gmail.com>
2014-11-24 13:34:43 -0500

    few more changes[

--- src/edu/neu/ccs/constants/Constants.java
+++ src/edu/neu/ccs/constants/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
 	public static final int START_YEAR = 1980;
 	public static final int END_YEAR = 2012;
 	public static final String YEAR_COUNTER_GRP = "YEAR";
-	public static final String UNIQUE_INDUSTRIES_KEY_TAG = "#uniqueIndustry#";
+	public static final String UNIQUE_INDUSTRIES_KEY_TAG = "UI";
 }
